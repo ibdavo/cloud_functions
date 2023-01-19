2c33845eed6bf54bc325b40017b95db7591f61cf
cah-david-schaffner <77859452+cah-david-schaffner@users.noreply.github.com>
2023-01-19 08:49:42 -0500

    in progress. todo: f3 subscription

--- f2/function2.py
+++ f2/function2.py
@@ -1,2 +1,6 @@
+from flask import Response
+
 def main_f2(request):
-    print(f"[{__name__}] Received request: {request}")
+    print(f"[{__name__}] Received request: {request.data}")
+
+    return Response("OK", status=200)
